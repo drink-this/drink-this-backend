d3c2c56b8b62c564595823ee6008841bda6d3fc2
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-07-29 15:18:29 +0200

    Adds session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+
+  def create
+    # Get access tokens from the google server
+    user = User.create_from_omniauth(auth_hash)
+
+    # Access_token is used to authenticate request made from the rails application to the google server
+    user.google_token = auth_hash.credentials.token
+
+    # Refresh_token to request new access_token
+    # Note: Refresh_token is only sent once during the first request
+    refresh_token = auth_hash.credentials.refresh_token
+
+    #Google assumes that you have stored the value and would not send it again.
+    user.google_refresh_token = refresh_token if refresh_token.present?
+
+    user.save
+
+    #cookies.encrypted[:current_user_id] = {value: user.id, expires: Time.now + 4.days }
+  end
+
+  def destroy
+    session[:user_id] = nil
+
+    redirect_to root_path
+  end
+
+  private
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+end
