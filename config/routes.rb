0f70edb03c41d33b6229cbdb463f93464c8f0a11
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-07-29 15:17:49 +0200

    Updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login', to: redirect('auth/auth/google_oauth2')
+  get '/logout', to: 'session#destroy'
+
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
 end
