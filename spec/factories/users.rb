23ec215e0ad336612d6fae653469ce437593a7d4
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-07-29 16:07:26 +0200

    Updates factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,5 @@ FactoryBot.define do
     uid { Faker::Number.uid(digits: 21) }
     name { Faker::GreekPhilosophers.name }
     email { Faker::Internet.email }
-    #password_digest { Faker::Internet.password(min_length: 8) }
   end
 end
