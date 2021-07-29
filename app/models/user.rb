7d8151e8c0b61986d0f70b827b82c3659efd9268
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-07-29 15:16:26 +0200

    Updates user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  validates :name, :email, :password_digest, presence: true
+  validates :name, :email, presence: true
   validates_uniqueness_of :email
 
   has_many :ratings
+
+  def self.create_from_omniauth(auth_hash)
+    where(email: auth_hash.info.email).first_or_initialize do |user|
+      user.uid = auth_hash.uid
+      user.name = auth_hash.info.name
+      user.email = auth_hash.info.email
+    end
+ end
 end
