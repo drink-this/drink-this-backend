42d1d7bde4c18603de622f41410d1b736fcfe284
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-28 20:07:18 -0600

    Create basic recommendation engine

--- app/services/recommendation_service.rb
+++ app/services/recommendation_service.rb
@@ -1,14 +1,38 @@
 require 'pycall/import'
-# require 'pandas'
+include PyCall::Import
+
+pyfrom :sklearn, import: :metrics
+pyfrom 'sklearn.metrics', import: :pairwise
+pyfrom 'sklearn.metrics.pairwise', import: :euclidean_distances
 
 class RecommendationService
-  include PyCall::Import
-
   def self.say_hello
-    # PyCall.import_module("pandas")
-    # df = Pandas.read_csv("cocktail_ratings.csv")
+    pandas = PyCall.import_module("pandas")
+    numpy = PyCall.import_module("numpy")
+    sklearn = PyCall.import_module("sklearn")
+    csv_data = Pandas.read_csv("./db/data/cocktail_ratings.csv") #replace with our ratings data
+    df = csv_data.set_index('name').fillna(0) #will likely be user_id instead of name
+    # euclidean = Numpy.round(sklearn.metrics.pairwise.euclidean_distances(df,df),2)
+    euclidean = Numpy.round(sklearn.metrics.pairwise.cosine_similarity(df,df),2)
+    similar = Pandas.DataFrame.new(data=euclidean, index=df.index,columns=df.index)
+    # sort true with Euclidean Distance
+    max = similar.loc['Max'].sort_values(0,ascending=false)[1..5] #here, Max is replacing our USER_ID for the user requesting the rec
+    df_max = Pandas.DataFrame.new(data=max, index=df.index)
+    pivoted = Pandas.melt(df.reset_index(),id_vars='name',value_vars=df.keys)
+    scraped_pivot = pivoted[pivoted.value != 0]
+    new = df_max.reset_index()
+    # total = new.merge(pivoted).dropna()
+    total = new.merge(scraped_pivot).dropna()
+    total['weightedRating']=total['Max']*total['value']
+    similarity_weighted = total.groupby('variable').sum()[['Max','weightedRating']]
+    empty = Pandas.DataFrame.new()
+    empty['weightedAvgRecScore'] = similarity_weighted['Max']/similarity_weighted['weightedRating']
+    # find an average rating by drink and multiply that by the 1/1+(ed)
+    # find a weighted avg of the val column * the Max column
+    # empty['variable'] = similarity_weighted.index
+    empty.sort_values(by='weightedAvgRecScore', ascending=false)
+    # empty.loc[empty['weightedAvgRecScore'].idxmax()]
+    recommendation = empty[empty.weightedAvgRecScore == empty.weightedAvgRecScore.max()]
     binding.pry
-    math = PyCall.import_module("math")
-    math.sin(math.pi / 4) - Math.sin(Math::PI / 4)
   end
 end
