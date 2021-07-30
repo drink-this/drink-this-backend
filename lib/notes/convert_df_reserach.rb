a29a05ffb11993ca658355f79dd7ca11f57a325f
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-29 18:35:12 -0600

    Take notes

--- lib/notes/convert_df_reserach.rb
+++ lib/notes/convert_df_reserach.rb
@@ -6,6 +6,12 @@ end
 
 df = Pandas.DataFrame.new(data = all_three_array)
 
-df_index = df.set_index(df[0]).
-df_index_drop = df_index.drop(df_index.columns[[0]], axis=1)
+df.columns=['user_id', 'cocktail_id', 'stars']
+
+df_pivot = df.pivot(index='user_id', columns='cocktail_id', values='stars')
+
+# df_index = df.set_index('user_id')
+
+# df_index = df.set_index(df[0])
+# df_index_drop = df_index.drop(df_index.columns[[0]], axis=1)
 # returned this warning => sys:1: FutureWarning: In a future version of pandas all arguments of DataFrame.drop except for the argument 'labels' will be keyword-only
