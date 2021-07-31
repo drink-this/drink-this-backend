3ed2d02e1eb279284218d7893e079fa9505fce31
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-31 11:58:42 -0600

    Complete successful test of creating rating response

--- spec/requests/api/v1/cocktail/rating_request_spec.rb
+++ spec/requests/api/v1/cocktail/rating_request_spec.rb
@@ -2,25 +2,32 @@ require 'rails_helper'
 
 RSpec.describe 'Rating API' do
   describe 'show response endpoint' do
+    before :all do
+      User.destroy_all
+      Cocktail.destroy_all
+      @cocktail_1 = create(:cocktail)
+      @user_1 = create(:user)
+    end
+
     it 'can send rating of a specific cocktail' do
-      cocktail_1 = create(:cocktail, name: 'margarita')
-      user_1 = create(:user)
-      cocktail_1_rating = ({
-        user: user_1,
-        cocktail: cocktail_1,
+      post "/api/v1/cocktails/#{@cocktail_1.id}/rating", params: {
+        user_id: @user_1.id,
+        cocktail_id: @cocktail_1.id,
         stars: 3
-        })
+      }
 
-      headers = { 'CONTENT_TYPE' => 'application/json' }
-      post "/api/v1/cocktails/#{cocktail_1.id}/rating", headers: headers, params: JSON.generate(rating: cocktail_1_rating)
-
-      expect(response).to be_successful
       expect(response.status).to eq(201)
 
-      created_rating = JSON.parse(response.body, symbolize_names: true)
+      cocktail_1_rating = JSON.parse(response.body, symbolize_names: true)
+      rating = Rating.find(cocktail_1_rating[:data][:id])
 
-      expect(created_rating).to have_key(:data)
-      expect(created_rating[:data]).to be_a Hash
+      expect(cocktail_1_rating).to have_key(:data)
+      expect(cocktail_1_rating[:data]).to be_a Hash
+
+      expect(cocktail_1_rating[:data][:type]).to eq("cocktail_rating")
+      expect(cocktail_1_rating[:data][:attributes][:user_id]).to eq(rating.user_id)
+      expect(cocktail_1_rating[:data][:attributes][:cocktail_id]).to eq(rating.cocktail_id)
+      expect(cocktail_1_rating[:data][:attributes][:stars]).to eq(rating.stars)
     end
   end
 end
