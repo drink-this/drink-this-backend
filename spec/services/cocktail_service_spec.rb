a3dd1fa380786be3c414fdaec7da95c731c87628
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-30 23:59:41 -0600

    Refactor cocktail service

--- spec/services/cocktail_service_spec.rb
+++ spec/services/cocktail_service_spec.rb
@@ -10,11 +10,21 @@ RSpec.describe CocktailService, :vcr do
 
           expect(manhattan).to be_a Hash
           expect(manhattan['idDrink']).to be_a String
+          expect(manhattan['idDrink']).to eq('11008')
+
           expect(manhattan['strDrink']).to be_a String
+          expect(manhattan['strDrink']).to eq('Manhattan')
           expect(manhattan['strInstructions']).to be_a String
+          expect(manhattan['strInstructions']).to eq('Stirred over ice, strained into a chilled glass, garnished, and served up.')
+
           expect(manhattan['strDrinkThumb']).to be_a String
+          expect(manhattan['strDrinkThumb']).to eq('https://www.thecocktaildb.com/images/media/drink/yk70e31606771240.jpg')
+
           expect(manhattan['strIngredient1']).to be_a String
+          expect(manhattan['strIngredient1']).to eq('Sweet Vermouth')
+
           expect(manhattan['strMeasure1']).to be_a String
+          expect(manhattan['strMeasure1']).to eq('3/4 oz ')
         end
       end
     end
