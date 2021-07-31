b03531a6759714deafdd446a9fe5a6b4e11ee4cd
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-31 12:00:05 -0600

    Implement create action

--- app/controllers/api/v1/cocktails/rating_controller.rb
+++ app/controllers/api/v1/cocktails/rating_controller.rb
@@ -1,4 +1,20 @@
 class Api::V1::Cocktails::RatingController < ApplicationController
   def create
+    rating = Rating.new(rating_params)
+    if rating.save
+      render json: rating, status: :created
+    else
+      # cannot_process(rating.errors.map { |_attr, msg| msg })
+      render json: { error: 'Failed to create resource', messages: rating.errors.full_messages },
+        status: :bad_request
+    end
+    # created_rating = Rating.create(rating_params)
+    # render json: RatingSerializer.new(created_rating)
+  end
+
+  private
+
+  def rating_params
+    params.permit(:user_id, :cocktail_id, :stars)
   end
 end
