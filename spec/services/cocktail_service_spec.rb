1e39d706d323cf209f86bf589e2ee415507083a0
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-31 12:15:49 -0600

    Refactor tests to symbolize keys

--- spec/services/cocktail_service_spec.rb
+++ spec/services/cocktail_service_spec.rb
@@ -9,22 +9,22 @@ RSpec.describe CocktailService, :vcr do
           manhattan = CocktailService.get_cocktail_details(11008)
 
           expect(manhattan).to be_a Hash
-          expect(manhattan['idDrink']).to be_a String
-          expect(manhattan['idDrink']).to eq('11008')
+          expect(manhattan[:idDrink]).to be_a String
+          expect(manhattan[:idDrink]).to eq('11008')
 
-          expect(manhattan['strDrink']).to be_a String
-          expect(manhattan['strDrink']).to eq('Manhattan')
-          expect(manhattan['strInstructions']).to be_a String
-          expect(manhattan['strInstructions']).to eq('Stirred over ice, strained into a chilled glass, garnished, and served up.')
+          expect(manhattan[:strDrink]).to be_a String
+          expect(manhattan[:strDrink]).to eq('Manhattan')
+          expect(manhattan[:strInstructions]).to be_a String
+          expect(manhattan[:strInstructions]).to eq('Stirred over ice, strained into a chilled glass, garnished, and served up.')
 
-          expect(manhattan['strDrinkThumb']).to be_a String
-          expect(manhattan['strDrinkThumb']).to eq('https://www.thecocktaildb.com/images/media/drink/yk70e31606771240.jpg')
+          expect(manhattan[:strDrinkThumb]).to be_a String
+          expect(manhattan[:strDrinkThumb]).to eq('https://www.thecocktaildb.com/images/media/drink/yk70e31606771240.jpg')
 
-          expect(manhattan['strIngredient1']).to be_a String
-          expect(manhattan['strIngredient1']).to eq('Sweet Vermouth')
+          expect(manhattan[:strIngredient1]).to be_a String
+          expect(manhattan[:strIngredient1]).to eq('Sweet Vermouth')
 
-          expect(manhattan['strMeasure1']).to be_a String
-          expect(manhattan['strMeasure1']).to eq('3/4 oz ')
+          expect(manhattan[:strMeasure1]).to be_a String
+          expect(manhattan[:strMeasure1]).to eq('3/4 oz ')
         end
       end
     end
