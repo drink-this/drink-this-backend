3499691e4f7c411c8e82534db24c391ba84f20eb
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-31 13:19:36 -0600

    Implement validation of stars as integer only

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,5 +2,5 @@ class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :cocktail
 
-  validates :stars, presence: true
+  validates :stars, presence: true, numericality: { only_integer: true }
 end
