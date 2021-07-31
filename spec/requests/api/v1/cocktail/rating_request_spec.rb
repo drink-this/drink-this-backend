f11926197bc3dde344e41413c8e569d0e0e497a6
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-31 13:18:26 -0600

    Create edge case test on stars as non-integer

--- spec/requests/api/v1/cocktail/rating_request_spec.rb
+++ spec/requests/api/v1/cocktail/rating_request_spec.rb
@@ -29,5 +29,31 @@ RSpec.describe 'Rating API' do
       expect(cocktail_1_rating[:data][:attributes][:cocktail_id]).to eq(rating.cocktail_id)
       expect(cocktail_1_rating[:data][:attributes][:stars]).to eq(rating.stars)
     end
+
+    it 'request must have params' do
+      post "/api/v1/cocktails/#{@cocktail_1.id}/rating", params: { }
+
+      expect(response.status).to eq(400)
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response_body[:error]).to eq("Failed to create resource")
+      expect(response_body[:messages].first).to eq("User must exist")
+      expect(response_body[:messages].second).to eq("Cocktail must exist")
+      expect(response_body[:messages].third).to eq("Stars can't be blank")
+    end
+
+    it 'request must have stars as an integer' do
+      post "/api/v1/cocktails/#{@cocktail_1.id}/rating", params: {
+        user_id: @user_1.id,
+        cocktail_id: @cocktail_1.id,
+        stars: "three"
+      }
+
+      expect(response.status).to eq(400)
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response_body[:error]).to eq("Failed to create resource")
+      expect(response_body[:messages].first).to eq("Stars is not a number")
+    end
   end
 end
