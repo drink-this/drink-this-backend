5c69c933d903b6e31968112e87b0c313c5457b2b
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-01 12:14:23 -0600

    Implement methods

---
+++ app/facades/cocktail_facade.rb
@@ -0,0 +1,53 @@
+class CocktailFacade
+  def self.retrieve_cocktail(cocktail_id)
+    response = CocktailService.get_cocktail_details(cocktail_id)
+
+    details = response[:drinks].first
+
+    ingredients = get_ingredients(details)
+    measurements = get_measurements(details)
+
+    recipe = measurements.zip(ingredients).map do |i|
+      i.join if i != ["", ""]
+    end.compact
+
+    rating = if Rating.find_by(cocktail_id: cocktail_id).present?
+      Rating.find_by(cocktail_id: cocktail_id).pluck(:stars)
+    else
+      0
+    end
+
+    cocktail = {
+      name: details[:strDrink],
+      thumbnail: details[:strDrinkThumb],
+      glass: details[:strGlass],
+      recipe: recipe,
+      instructions: details[:strInstructions],
+      rating: rating
+    }
+  end
+
+  def self.get_ingredients(cocktail_data)
+    ingredients = []
+    cocktail_data.each do |key, value|
+      stringify_key = key.to_s
+      if stringify_key.include?("Ingredient")
+        value = "" if value.nil?
+        ingredients << value
+      end
+    end
+    ingredients
+  end
+
+  def self.get_measurements(cocktail_data)
+    measurements = []
+    cocktail_data.each do |key, value|
+      stringify_key = key.to_s
+      if stringify_key.include?("Measure")
+        value = "" if value.nil?
+        measurements << value
+      end
+    end
+    measurements
+  end
+end
