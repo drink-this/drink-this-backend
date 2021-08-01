83e3d54963eaf79fe3123a84b0cf5aaa92e5236f
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-01 13:50:32 -0600

    Add cocktail details serializer

---
+++ app/serializers/cocktail_serializer.rb
@@ -0,0 +1,14 @@
+class CocktailSerializer < ActiveModel::Serializer
+  attributes :name, :thumbnail, :glass, :recipe, :instructions, :rating
+
+  def self.details(id, cocktail)
+    details = {
+      data: {
+        id: id,
+        type: 'cocktail',
+        attributes: cocktail
+      }
+    }
+    details.to_json
+  end
+end
