fbc78edff4fe57a4359cb57fa4e7bf63f5c50e11
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-01 13:51:57 -0600

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :cocktails, only: :show
+      
       namespace :cocktails do
         scope '/:id' do
           resources :rating, only: :create
