0d157af3364c21c40e282f4c0b56c57e5da8158a
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-01 20:02:17 -0600

    Implement search cocktails class method

--- app/services/cocktail_service.rb
+++ app/services/cocktail_service.rb
@@ -8,9 +8,22 @@ class CocktailService
     parse_json(response)
   end
 
-  private
+  def self.search_cocktails(query)
+    response = Faraday.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?') do |req|
+      req.params['s'] = query
+      req.params['api_key'] = ENV['cocktail_key']
+    end
+
+    parse_json(response)
+  end
+
+  # def self.conn
+  #   Faraday.new(url: 'https://www.thecocktaildb.com/api/json/v1/1')
+  # end
 
   def self.parse_json(response)
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  # private_class_method :conn, :parse_json
 end
