dd52e0c72a60b11c5fe88ebc95797255be521417
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-02 15:54:01 -0600

    Implement conditionals to pass sad paths

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::CocktailsController < ApplicationController
   def show
-    cocktail = CocktailFacade.retrieve_cocktail(params[:id])
-    render json: CocktailDetailsSerializer.details(params[:id], cocktail)
+    if params[:id].nil?
+      render json: { error: 'No cocktail currently exist.' }
+    else
+      if params[:user_id].nil? || Rating.find_by(cocktail_id: params[:id], user_id: params[:user_id]).nil?
+        render json: { error: 'User error.'}
+      else
+        cocktail = CocktailFacade.retrieve_cocktail(params[:id])
+        render json: CocktailDetailsSerializer.details(params[:id], cocktail)
+      end
+    end
   end
 end
