c6d07d3c8037ca264896793354c37abac289c67a
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-02 13:25:30 -0600

    Complete test for recommendation request

--- spec/requests/api/v1/cocktails/recommendation_request_spec.rb
+++ spec/requests/api/v1/cocktails/recommendation_request_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe '/recommendation', :vcr do
         cocktail_1 = create(:cocktail, name: 'margarita')
         cocktail_2 = create(:cocktail, name: 'old fashion')
         cocktail_3 = create(:cocktail, name: 'whisky sour')
-        cocktail_4 = create(:cocktail, name: 'godfather')
+        cocktail_4 = create(:cocktail, name: 'godfather', id: 11423)
         cocktail_5 = create(:cocktail, name: 'long island')
 
         user_1 = create(:user)
@@ -40,24 +40,30 @@ RSpec.describe '/recommendation', :vcr do
         result = JSON.parse(response.body, symbolize_names: true)
 
         expect(result[:data]).to be_a Hash
-        expect(result[:data][:id]).to eq(@cocktail_1.id.to_s)
+        expect(result[:data][:id]).to eq(cocktail_3.id.to_s)
         expect(result[:data][:type]).to eq('cocktail')
 
         attributes = result[:data][:attributes]
 
         expect(attributes).to be_a Hash
         expect(attributes).to have_key :name
-        expect(attributes[:name]).to eq('Vodka Fizz')
+        expect(attributes[:name]).to eq('Godfather')
 
         expect(attributes).to have_key :thumbnail
-        expect(attributes).to have_key :glass
-        expect(attributes).to have_key :recipe
-        expect(attributes).to have_key :instructions
-        expect(attributes).to have_key :rating
-        expect(attributes[:rating]).to eq(4)
-      end
+        expect(attributes[:thumbnail]).to eq 'https://www.thecocktaildb.com/images/media/drink/e5zgao1582582378.jpg'
 
-      
+        expect(attributes).to have_key :glass
+        expect(attributes[:glass]).to eq 'Old-fashioned glass'
+
+        expect(attributes).to have_key :recipe
+        expect(attributes[:recipe]).to eq(['1 1/2 oz Scotch','3/4 oz Amaretto'])
+
+        expect(attributes).to have_key :instructions
+        expect(attributes[:instructions]).to eq 'Pour all ingredients directly into old fashioned glass filled with ice cubes. Stir gently.'
+
+        expect(attributes).to have_key :rating
+        expect(attributes[:rating]).to eq(0)
+      end 
     end
   end
 end
