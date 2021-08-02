413a3718d9c3144ca95248d480b12205f2b0b3c6
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-02 15:52:13 -0600

    Create sad paths for cocktail details request

--- spec/requests/api/v1/cocktail/cocktail_request_spec.rb
+++ spec/requests/api/v1/cocktail/cocktail_request_spec.rb
@@ -9,13 +9,13 @@ RSpec.describe 'Cocktail Details API', :vcr do
         Rating.destroy_all
         @cocktail_1 = create(:cocktail, id: 16967)
         @user_1 = create(:user)
+        @user_2 = create(:user)
         create(:rating, cocktail_id: @cocktail_1.id, user_id: @user_1.id, stars: 4)
       end
 
       it 'send json of cocktail details via get request' do
         get "/api/v1/cocktails/#{@cocktail_1.id}", params: {
-          user_id: @user_1.id,
-          cocktail_id: @cocktail_1.id
+          user_id: @user_1.id
         }
 
         expect(response.status).to eq(200)
@@ -38,6 +38,40 @@ RSpec.describe 'Cocktail Details API', :vcr do
         expect(attributes).to have_key :instructions
         expect(attributes).to have_key :rating
         expect(attributes[:rating]).to eq(4)
+      end
+
+      it '(sad path) send json error when no params with user id' do
+        get "/api/v1/cocktails/#{@cocktail_1.id}", params: { }
+
+        expect(response.status).to eq(200)
+
+        result = JSON.parse(response.body, symbolize_names: true)
+
+        expect(result[:error]).to eq('User error.')
+      end
+
+      it '(sad path) send json error when no params with user id' do
+        get "/api/v1/cocktails/#{@cocktail_1.id}", params: {
+          user_id: @user_2.id
+        }
+
+        expect(response.status).to eq(200)
+
+        result = JSON.parse(response.body, symbolize_names: true)
+
+        expect(result[:error]).to eq('User error.')
+      end
+
+      it '(sad path) send json error when there is an incorrect cocktail id' do
+        get "/api/v1/cocktails/38293", params: {
+          user_id: @user_1.id
+        }
+
+        expect(response.status).to eq(200)
+
+        result = JSON.parse(response.body, symbolize_names: true)
+
+        expect(result[:error]).to eq('User error.')
       end
     end
   end
