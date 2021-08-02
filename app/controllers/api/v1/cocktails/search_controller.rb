7ad1bc7d9edad143ce0f0daaa94b1698ddfa0046
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-02 17:32:43 -0600

    Add conditional

--- app/controllers/api/v1/cocktails/search_controller.rb
+++ app/controllers/api/v1/cocktails/search_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::Cocktails::SearchController < ApplicationController
   def index
-    results = CocktailFacade.retrieve_search_results(params[:search], params[:user_id])
-    render json: CocktailSearchSerializer.search_list(results)
+    if params[:search].nil? || params[:search].empty?
+      render json: { data: { error: 'Search query not valid.'} }
+    else
+      results = CocktailFacade.retrieve_search_results(params[:search], params[:user_id])
+      render json: CocktailSearchSerializer.search_list(results)
+    end
   end
 end
