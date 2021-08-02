af312913ec5e13bbfc6133a0f9545d21c5d03fa0
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-02 12:48:11 -0600

    Update model to use a15 percent similar users and repair calculation error

--- app/services/recommendation_service.rb
+++ app/services/recommendation_service.rb
@@ -15,23 +15,25 @@ class RecommendationService
   end
 
   def self.weighted_avg(new_dataframe, unrated_cocktails, user_id)
-    new_dataframe['weightedAvgRecScore'] = unrated_cocktails[user_id]/unrated_cocktails['weightedRating']
+    new_dataframe['weightedAvgRecScore'] = unrated_cocktails['sum_weightedRating']/unrated_cocktails['count_ratings']
   end
 
-  def self.remove_rated_cocktails(pivoted_ratings, combined_ratings, user_id)
+  def self.remove_rated_cocktails(pivoted_ratings, all_of_it, user_id)
     user_pivot = pivoted_ratings[pivoted_ratings.user_id == user_id]
-    reset_sw = combined_ratings.reset_index()
+    reset_sw = all_of_it.reset_index()
     unrated_cocktails = reset_sw.merge(user_pivot).set_index('cocktail_id')
     unrated_cocktails = unrated_cocktails[unrated_cocktails.value == 0]
   end
 
   def self.weighted_ratings(baseline_ratings, user_id)
-    baseline_ratings['weightedRating'] = (1 / baseline_ratings[user_id] + 1) * baseline_ratings['value']
+    baseline_ratings[user_id] = (1 / (baseline_ratings[user_id] + 1))
+    baseline_ratings['weightedRating'] = baseline_ratings[user_id] * baseline_ratings['value']
   end
 
   def self.user_distances(user_distance_matrix, df, user_id)
     other_users = user_distance_matrix.loc[user_id].sort_values(0,ascending=true)
-    user_removed = other_users[other_users>0][0..4]
+    cap = (other_users.size * 0.15).ceil
+    user_removed = other_users[other_users>0][0..cap]
     Pandas.DataFrame.new(data=user_removed, index=df.index)
   end
 
@@ -45,7 +47,6 @@ class RecommendationService
     df = Dataframeable.create_df
 
     user_distance_matrix = euclidean_distance(df)
-
     distances_from_user = user_distances(user_distance_matrix, df, user_id)
 
     cocktail_ratings = Pandas.melt(df.reset_index(),id_vars='user_id',value_vars=df.keys)
@@ -55,14 +56,20 @@ class RecommendationService
 
     weighted_ratings(baseline_ratings, user_id)
 
+    counts = baseline_ratings.groupby('cocktail_id').agg('count')['value']
+
     combined_ratings = baseline_ratings.groupby('cocktail_id').sum()[[user_id,'weightedRating']]
 
-    unrated_cocktails = remove_rated_cocktails(cocktail_ratings, combined_ratings, user_id)
+    all_of_it = combined_ratings.join(counts)
+
+    all_of_it.columns = ['similarity', 'sum_weightedRating','count_ratings']
+
+    unrated_cocktails = remove_rated_cocktails(cocktail_ratings, all_of_it, user_id)
 
     new_dataframe = Pandas.DataFrame.new()
 
     weighted_avg(new_dataframe, unrated_cocktails, user_id)
 
-    return_recommendation(new_dataframe)
+    return_recommendation(new_dataframe).reset_index['cocktail_id'].to_i
   end
 end
