7046c6abc41e456820f13583893a5c9d10f98043
Richard <rdesilvey@gmail.com>
2021-08-03 10:45:30 -0600

    Test for empty rather than nil
    Where clause will return an instance of AR relation which is always truthy regardless of what it found.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,7 +2,7 @@ class AuthenticationController < ApplicationController
   def token_auth
     token = params[:auth_token]
     payload = GoogleService.decode(token)
-    if User.where('email = ?', payload[:email])
+    if !User.where('email = ?', payload[:email]).empty?
       render json: {is_new: false, token: token}
     else
       User.create!(name: payload[:name], email: payload[:email], google_token: token)
