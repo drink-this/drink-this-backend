94fffff6bdc7b5fa711ca2cc0f93435eaef6a43e
Richard <rdesilvey@gmail.com>
2021-08-03 10:55:12 -0600

    Clean up statement, use find_by

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,7 +2,7 @@ class AuthenticationController < ApplicationController
   def token_auth
     token = params[:auth_token]
     payload = GoogleService.decode(token)
-    if !User.where('email = ?', payload[:email]).empty?
+    unless User.find_by(email: payload[:email]).nil?
       render json: { is_new: false, token: token }
     else
       User.create!(name: payload[:name], email: payload[:email], google_token: token)
