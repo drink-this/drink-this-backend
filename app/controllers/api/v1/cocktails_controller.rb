f3012417f213bd246d7bd59b1b508265bd9685c3
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-02 20:08:01 -0600

    Pull latest

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -1,6 +1,18 @@
 class Api::V1::CocktailsController < ApplicationController
+  before_action :authorize_user
+  
   def show
     cocktail = CocktailFacade.retrieve_cocktail(params[:id])
     render json: CocktailDetailsSerializer.details(params[:id], cocktail)
+  end
+
+  def authorize_user
+    return if current_user
+
+    render json: { errors: "Couldn't find User" }, status: 404
+  end
+
+  def current_user
+    @current_user ||= User.find_by(google_token: params[:auth_token]) if params[:auth_token]
   end
 end
