7ec8b6fc1a1d7efda3c094e22c33a5b50bf7b610
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-02 19:59:54 -0600

    Remove uid from user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :user do
-    uid { Faker::Number.number(digits: 21) }
     name { Faker::GreekPhilosophers.name }
     email { Faker::Internet.email }
     google_token { Faker::Blockchain::Ethereum.address }
