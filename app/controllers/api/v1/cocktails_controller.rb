c3ddb34d31715a3425cd377a5b250d73be8cb0cd
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-02 22:51:55 -0600

    Refactor conditionals

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -2,11 +2,12 @@ class Api::V1::CocktailsController < Api::V1::AuthorizationController
   def show
     user = User.find_by(google_token: params[:auth_token])
 
-    if params[:id].nil? || !params[:id].present? || params[:id].empty?
+    if !params[:id].present?
       render json: { errors: "Couldn't find Cocktail" }, status: 404
     else
       cocktail = CocktailFacade.retrieve_cocktail(user.id, params[:id])
-      if cocktail[:error].present?
+
+      if cocktail == false
         render json: { errors: "Couldn't find Cocktail"}, status: 404
       else
         render json: CocktailDetailsSerializer.details(params[:id], cocktail)
