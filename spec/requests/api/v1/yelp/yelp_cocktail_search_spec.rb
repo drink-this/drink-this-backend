8a7c6785e7401b7624158bada7dadf4ad04945f9
Taija <taija.warbelow@gmail.com>
2021-08-03 16:30:00 -0700

    remove comments, correct expectations

--- spec/requests/api/v1/yelp/yelp_cocktail_search_spec.rb
+++ spec/requests/api/v1/yelp/yelp_cocktail_search_spec.rb
@@ -17,37 +17,12 @@ RSpec.describe 'Yelp Cocktail Search API' do
 
       first_result = search_results[:data].first
 
-      expect(first_result[:type]).to eq('yelp_cocktail_search')
+      expect(first_result[:type]).to eq('yelp_search_result')
       expect(first_result[:attributes][:name]).to be_a String
-      expect(first_result[:attributes][:address]).to be_a String
+      expect(first_result[:attributes][:address]).to be_a Array
       expect(first_result[:attributes][:business_type]).to be_an Array
       expect(first_result[:attributes][:thumbnail]).to be_a String
       expect(first_result[:attributes][:yelp_link]).to be_a String
     end
   end
 end
-
-
-# When a client sends a get request to "/api/v1/search/yelp", with a valid param in request body called "query" of type string, the response would be the results:
-#
-# {
-# "data": [ {
-#    "type"; "yelp cocktail search"
-#    "attributes": {
-  #    "name": "business name",
-  #    "address": "business address",
-  #    "business_type": "type of business",
-  #    "thumbnail": "business thumbnail",
-  #    "yelp_link": "yelp link to business"
-  # }
-# } ]
-# }
-
-
-# request headers
-  # Authorization: "Bearer #{YELP_KEY}"
-# params
-  # term: "manhattan"
-  # location: "san francisco"
-  # limit: 10
-  # categories: 'bars'
