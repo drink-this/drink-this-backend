e2bccbd42ea069ebb9d5da88596aec31d18568c5
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-02 19:53:00 -0600

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2021_08_03_003329) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "google_token"
