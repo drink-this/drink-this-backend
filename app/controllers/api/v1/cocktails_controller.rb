33325075b9797a7be4e7253a35ecd52ffa7c51b9
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-02 21:22:36 -0600

    Restructure controllers to add an auth controller

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -1,6 +1,6 @@
-class Api::V1::CocktailsController < ApplicationController
+class Api::V1::CocktailsController < Api::V1::AuthorizationController
   def show
-    cocktail = CocktailFacade.retrieve_cocktail(params[:id])
+    cocktail = CocktailFacade.retrieve_cocktail(current_user.id, params[:id])
     render json: CocktailDetailsSerializer.details(params[:id], cocktail)
   end
 end
--- app/controllers/api/v1/recommendations_controller.rb
+++ app/controllers/api/v1/recommendations_controller.rb
@@ -1,19 +1,7 @@
-class Api::V1::RecommendationsController < ApplicationController
-  before_action :authorize_user
-
+class Api::V1::RecommendationsController < Api::V1::AuthorizationController
   def show
     cocktail_id = RecommendationService.recommendation(current_user.id)
     details = CocktailFacade.retrieve_cocktail(current_user.id, cocktail_id)
     render json: CocktailDetailsSerializer.details(cocktail_id, details)
-  end
-
-  def authorize_user
-    return if current_user
-
-    render json: { errors: "Couldn't find User" }, status: :not_found
-  end
-
-  def current_user
-    @current_user ||= User.find_by(google_token: params[:auth_token]) if params[:auth_token]
   end
 end
