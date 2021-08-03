81baa5ae546f74fad8939a6956d0c6b6bc55b3ab
Taija <taija.warbelow@gmail.com>
2021-08-03 16:13:56 -0700

    initialize method with TODO for refactor

---
+++ app/poros/business.rb
@@ -0,0 +1,11 @@
+class Business
+  attr_reader :name, :address, :business_type, :thumbnail, :yelp_link
+  def initialize(result)
+    @name = result[:name]
+    @address = result[:location][:display_address]
+    # TODO will want to refine @business_type so it is only getting the titles, not the aliases
+    @business_type = result[:categories]
+    @thumbnail = result[:image_url]
+    @yelp_link = result[:url]
+  end
+end
