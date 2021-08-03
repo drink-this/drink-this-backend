59d01bfdc593631c0598f63cafa1f6879f043a1f
Taija <taija.warbelow@gmail.com>
2021-08-03 16:10:51 -0700

    get businesses method, private conn method

---
+++ app/services/yelp_service.rb
@@ -0,0 +1,14 @@
+class YelpService
+  def self.get_businesses(cocktail, location)
+    response = conn.get("businesses/search?term=#{cocktail}&location=#{location}&limit=10&categories=bars")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new(url: "https://api.yelp.com/v3/") do |faraday|
+      faraday.headers['Authorization'] = "Bearer #{ENV['YELP_KEY']}"
+    end
+  end
+
+  private_class_method :conn
+end
