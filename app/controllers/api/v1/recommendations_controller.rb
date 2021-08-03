238f7ad9b6d6f7a2770d5338bf8d1e28a390df39
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-02 20:41:08 -0600

    Remove comment

--- app/controllers/api/v1/recommendations_controller.rb
+++ app/controllers/api/v1/recommendations_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::RecommendationsController < ApplicationController
   def show
     cocktail_id = RecommendationService.recommendation(current_user.id)
     details = CocktailFacade.retrieve_cocktail(current_user.id, cocktail_id)
-    # details = CocktailFacade.retrieve_cocktail(cocktail_id)
     render json: CocktailDetailsSerializer.details(cocktail_id, details)
   end
 
