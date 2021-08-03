e92634da1526835c6f4a8586508d38e14567d8ad
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-02 19:02:23 -0600

    Add sad path test for unauthenticated user

--- spec/requests/api/v1/cocktails/recommendation_request_spec.rb
+++ spec/requests/api/v1/cocktails/recommendation_request_spec.rb
@@ -3,67 +3,83 @@ require 'rails_helper'
 RSpec.describe '/recommendation', :vcr do
   VCR.use_cassette('cocktail drink', :record => :new_episodes) do
     describe 'GET /recommendation' do
-      it 'provides a random cocktail for the first user'
+      
+      context 'user is not logged in' do
+        it 'returns an error message and 404 status code' do
+          get "/api/v1/recommendation"
 
-      it 'returns a recommendation based on other user data' do
-        cocktail_1 = create(:cocktail, name: 'margarita')
-        cocktail_2 = create(:cocktail, name: 'old fashion')
-        cocktail_3 = create(:cocktail, name: 'whisky sour')
-        cocktail_4 = create(:cocktail, name: 'godfather', id: 11423)
-        cocktail_5 = create(:cocktail, name: 'long island')
+          expect(response).to have_http_status 404
+          expect(response.body).to match(/Couldn't find User/)
+        end
+      end
+      
+      context 'user is logged in' do
+        it 'provides a random cocktail if only one user exists'
 
-        user_1 = create(:user)
-        user_2 = create(:user)
-        user_3 = create(:user)
-        user_4 = create(:user)
+        it 'provides a random cocktail if no similar (not identical) users exist'
 
-        create(:rating, user: user_1, cocktail: cocktail_1, stars: 2)
-        create(:rating, user: user_1, cocktail: cocktail_2, stars: 2)
-        create(:rating, user: user_1, cocktail: cocktail_3, stars: 2)
+        it 'returns a recommendation based on other user data' do
+          cocktail_1 = create(:cocktail, name: 'margarita')
+          cocktail_2 = create(:cocktail, name: 'old fashion')
+          cocktail_3 = create(:cocktail, name: 'whisky sour')
+          cocktail_4 = create(:cocktail, name: 'godfather', id: 11423)
+          cocktail_5 = create(:cocktail, name: 'long island')
 
-        create(:rating, user: user_2, cocktail: cocktail_1, stars: 5)
-        create(:rating, user: user_2, cocktail: cocktail_2, stars: 5)
-        create(:rating, user: user_2, cocktail: cocktail_3, stars: 2)
-        create(:rating, user: user_2, cocktail: cocktail_4, stars: 1)
-        create(:rating, user: user_2, cocktail: cocktail_5, stars: 1)
+          user_1 = create(:user)
+          user_2 = create(:user)
+          user_3 = create(:user, google_token: "agnasdgn3r9n240unrfsdf")
+          user_4 = create(:user)
 
-        create(:rating, user: user_3, cocktail: cocktail_1, stars: 2)
+          create(:rating, user: user_1, cocktail: cocktail_1, stars: 2)
+          create(:rating, user: user_1, cocktail: cocktail_2, stars: 2)
+          create(:rating, user: user_1, cocktail: cocktail_3, stars: 2)
 
-        create(:rating, user: user_4, cocktail: cocktail_1, stars: 5)
-        create(:rating, user: user_4, cocktail: cocktail_3, stars: 2)
-        create(:rating, user: user_4, cocktail: cocktail_4, stars: 1)
+          create(:rating, user: user_2, cocktail: cocktail_1, stars: 5)
+          create(:rating, user: user_2, cocktail: cocktail_2, stars: 5)
+          create(:rating, user: user_2, cocktail: cocktail_3, stars: 2)
+          create(:rating, user: user_2, cocktail: cocktail_4, stars: 1)
+          create(:rating, user: user_2, cocktail: cocktail_5, stars: 1)
 
-        get "/api/v1/recommendation", params: {auth_token: 'sagkjsdwnkvf'}
+          create(:rating, user: user_3, cocktail: cocktail_1, stars: 2)
 
-        expect(response.status).to eq(200)
+          create(:rating, user: user_4, cocktail: cocktail_1, stars: 5)
+          create(:rating, user: user_4, cocktail: cocktail_3, stars: 2)
+          create(:rating, user: user_4, cocktail: cocktail_4, stars: 1)
 
-        result = JSON.parse(response.body, symbolize_names: true)
+          allow_any_instance_of(User).to receive(:find_by).and_return(user_3)
 
-        expect(result[:data]).to be_a Hash
-        expect(result[:data][:id]).to eq(cocktail_3.id.to_s)
-        expect(result[:data][:type]).to eq('cocktail')
+          get "/api/v1/recommendation", params: {auth_token: 'agnasdgn3r9n240unrfsdf'}
 
-        attributes = result[:data][:attributes]
+          expect(response.status).to eq(200)
 
-        expect(attributes).to be_a Hash
-        expect(attributes).to have_key :name
-        expect(attributes[:name]).to eq('Godfather')
+          result = JSON.parse(response.body, symbolize_names: true)
 
-        expect(attributes).to have_key :thumbnail
-        expect(attributes[:thumbnail]).to eq 'https://www.thecocktaildb.com/images/media/drink/e5zgao1582582378.jpg'
+          expect(result[:data]).to be_a Hash
+          expect(result[:data][:id]).to eq(cocktail_3.id.to_s)
+          expect(result[:data][:type]).to eq('cocktail')
 
-        expect(attributes).to have_key :glass
-        expect(attributes[:glass]).to eq 'Old-fashioned glass'
+          attributes = result[:data][:attributes]
 
-        expect(attributes).to have_key :recipe
-        expect(attributes[:recipe]).to eq(['1 1/2 oz Scotch','3/4 oz Amaretto'])
+          expect(attributes).to be_a Hash
+          expect(attributes).to have_key :name
+          expect(attributes[:name]).to eq('Godfather')
 
-        expect(attributes).to have_key :instructions
-        expect(attributes[:instructions]).to eq 'Pour all ingredients directly into old fashioned glass filled with ice cubes. Stir gently.'
+          expect(attributes).to have_key :thumbnail
+          expect(attributes[:thumbnail]).to eq 'https://www.thecocktaildb.com/images/media/drink/e5zgao1582582378.jpg'
 
-        expect(attributes).to have_key :rating
-        expect(attributes[:rating]).to eq(0)
-      end 
+          expect(attributes).to have_key :glass
+          expect(attributes[:glass]).to eq 'Old-fashioned glass'
+
+          expect(attributes).to have_key :recipe
+          expect(attributes[:recipe]).to eq(['1 1/2 oz Scotch','3/4 oz Amaretto'])
+
+          expect(attributes).to have_key :instructions
+          expect(attributes[:instructions]).to eq 'Pour all ingredients directly into old fashioned glass filled with ice cubes. Stir gently.'
+
+          expect(attributes).to have_key :rating
+          expect(attributes[:rating]).to eq(0)
+        end    
+      end
     end
   end
 end
