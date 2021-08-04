8196a7eb5d2ae4bce9a8f8d937a5e010acb03921
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-03 18:19:31 -0600

    Update dataframeable module for clarity

--- app/modules/dataframeable.rb
+++ app/modules/dataframeable.rb
@@ -1,19 +1,8 @@
 module Dataframeable
 
   def self.create_df
-    # all_three = Rating.all.select('user_id, cocktail_id, stars')
-
-    # all_three_array = all_three.map do |three|
-    #   [three.user_id, three.cocktail_id, three.stars]
-    # end
-    ratings = Rating.prep_dataframe
-
-    df = Pandas.DataFrame.new(data = ratings)
-
+    df = Pandas.DataFrame.new(data = Rating.prep_dataframe)
     df.columns=['user_id', 'cocktail_id', 'stars']
-    
-    df_pivot = df.pivot(index='user_id', columns='cocktail_id', values='stars')
-
-    df_pivot.fillna(0)
+    df_pivot = df.pivot(index='user_id', columns='cocktail_id', values='stars').fillna(0)
   end
 end
