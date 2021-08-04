3375a4b6d14a976c10497829adeacf8ff586edaf
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-03 21:42:30 -0600

    Standardize error notation to singular

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -1,14 +1,12 @@
 class Api::V1::CocktailsController < Api::V1::AuthorizationController
   def show
-    user = User.find_by(google_token: params[:auth_token])
-
     if !params[:id].present?
-      render json: { errors: "Couldn't find Cocktail" }, status: :not_found
+      render json: { error: "Couldn't find Cocktail" }, status: :not_found
     else
-      cocktail = CocktailFacade.retrieve_cocktail(user.id, params[:id])
+      cocktail = CocktailFacade.retrieve_cocktail(current_user.id, params[:id])
 
       if cocktail == false
-        render json: { errors: "Couldn't find Cocktail" }, status: :not_found
+        render json: { error: "Couldn't find Cocktail" }, status: :not_found
       else
         render json: CocktailDetailsSerializer.details(params[:id], cocktail)
       end
