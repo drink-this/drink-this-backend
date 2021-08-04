183c76d1af6f1e98fe476450f916e6679f9e3b09
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-03 21:48:32 -0600

    Create working workflow for creating cocktail on rate

--- app/controllers/api/v1/cocktails/rating_controller.rb
+++ app/controllers/api/v1/cocktails/rating_controller.rb
@@ -1,21 +1,23 @@
 class Api::V1::Cocktails::RatingController < Api::V1::AuthorizationController
   def create
-    # require 'pry'; binding.pry
+    create_cocktail(params[:id]) if Cocktail.where(id: params[:id]).empty?
     rating = Rating.new(user_id: current_user.id, cocktail_id: params[:id], stars: params[:stars])
     if rating.save
       render json: rating, status: :created
     else
-      # cannot_process(rating.errors.map { |_attr, msg| msg })
       render json: { error: 'Failed to create resource', messages: rating.errors.full_messages },
              status: :bad_request
     end
-    # created_rating = Rating.create(rating_params)
-    # render json: RatingSerializer.new(created_rating)
   end
 
-  private
-
-  def rating_params
-    params.permit(:user_id, :cocktail_id, :stars)
+  def create_cocktail(cocktail_id)
+    details = CocktailFacade.retrieve_cocktail(current_user.id, cocktail_id)
+    Cocktail.create(id: cocktail_id, name: details[:name], thumbnail: details[:thumbnail]) unless details == false
   end
+
+  # private
+
+  # def rating_params
+  #   params.permit(:user_id, :cocktail_id, :stars)
+  # end
 end
