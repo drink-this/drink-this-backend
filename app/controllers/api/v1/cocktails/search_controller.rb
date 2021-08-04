58489087ca8048f4b9a5a384d712ce7d16506148
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-03 22:04:04 -0600

    Refactor search controller index

--- app/controllers/api/v1/cocktails/search_controller.rb
+++ app/controllers/api/v1/cocktails/search_controller.rb
@@ -3,13 +3,16 @@ class Api::V1::Cocktails::SearchController < Api::V1::AuthorizationController
     if !params[:search].present?
       render json: { error: "Couldn't find Cocktail" }, status: :not_found
     else
-      results = CocktailFacade.retrieve_search_results(params[:search], current_user.id)
+      search_results(params[:search], current_user.id)
+    end
+  end
 
-      if results == false
-        render json: { error: "Couldn't find Cocktail" }, status: :not_found
-      else
-        render json: CocktailSearchSerializer.search_list(results)
-      end
+  def search_results(query, user_id)
+    results = CocktailFacade.retrieve_search_results(query, user_id)
+    if results == false
+      render json: { error: "Couldn't find Cocktail" }, status: :not_found
+    else
+      render json: CocktailSearchSerializer.search_list(results)
     end
   end
 end
