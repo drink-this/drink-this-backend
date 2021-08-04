d599579a3e90af2df4d358969837c1d73cdc1d4d
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-03 18:32:08 -0600

    Add tests for updated ratings endpoint

--- spec/requests/api/v1/cocktails/rating_request_spec.rb
+++ spec/requests/api/v1/cocktails/rating_request_spec.rb
@@ -6,69 +6,101 @@ RSpec.describe 'Rating API' do
       User.destroy_all
       Cocktail.destroy_all
       @cocktail_1 = create(:cocktail)
-      @user_1 = create(:user)
+      @user_1 = create(:user, google_token: 'BBBasdsgergn240unrfs35253')
     end
 
-    it 'can send rating of a specific cocktail' do
-      post "/api/v1/cocktails/#{@cocktail_1.id}/rating/", params: {
-        user_id: @user_1.id,
-        cocktail_id: @cocktail_1.id,
-        stars: 3
-      }
+    context 'the cocktail exists in our database' do
+      it 'can send rating of a specific cocktail' do
+        post "/api/v1/cocktails/#{@cocktail_1.id}/rating/", params: {
+          auth_token: 'BBBasdsgergn240unrfs35253',
+          stars: 3
+        }
 
-      expect(response.status).to eq(201)
+        expect(response.status).to eq(201)
 
-      cocktail_1_rating = JSON.parse(response.body, symbolize_names: true)
-      rating = Rating.find(cocktail_1_rating[:data][:id])
+        cocktail_1_rating = JSON.parse(response.body, symbolize_names: true)
+        rating = Rating.find(cocktail_1_rating[:data][:id])
 
-      expect(cocktail_1_rating).to have_key(:data)
-      expect(cocktail_1_rating[:data]).to be_a Hash
+        expect(cocktail_1_rating).to have_key(:data)
+        expect(cocktail_1_rating[:data]).to be_a Hash
 
-      expect(cocktail_1_rating[:data][:type]).to eq("cocktail_rating")
-      expect(cocktail_1_rating[:data][:attributes][:user_id]).to eq(rating.user_id)
-      expect(cocktail_1_rating[:data][:attributes][:cocktail_id]).to eq(rating.cocktail_id)
-      expect(cocktail_1_rating[:data][:attributes][:stars]).to eq(rating.stars)
+        expect(cocktail_1_rating[:data][:type]).to eq("cocktail_rating")
+        expect(cocktail_1_rating[:data][:attributes][:user_id]).to eq(rating.user_id)
+        expect(cocktail_1_rating[:data][:attributes][:cocktail_id]).to eq(rating.cocktail_id)
+        expect(cocktail_1_rating[:data][:attributes][:stars]).to eq(rating.stars)
+      end
+
+      it 'request must have stars param' do
+        post "/api/v1/cocktails/#{@cocktail_1.id}/rating", params: { auth_token: 'BBBasdsgergn240unrfs35253' }
+
+        expect(response.status).to eq(400)
+        response_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response_body[:error]).to eq("Failed to create resource")
+        expect(response_body[:messages].first).to eq("User must exist")
+        expect(response_body[:messages].second).to eq("Cocktail must exist")
+        expect(response_body[:messages].third).to eq("Stars can't be blank")
+      end
+
+      it 'request must have auth_token param' do
+        post "/api/v1/cocktails/#{@cocktail_1.id}/rating", params: { stars: 3 }
+
+        expect(response.status).to eq(400)
+        response_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response_body[:error]).to eq("Failed to create resource")
+        expect(response_body[:messages].first).to eq("User must exist")
+        expect(response_body[:messages].second).to eq("Cocktail must exist")
+        expect(response_body[:messages].third).to eq("Stars can't be blank")
+      end
+
+      it 'request must have stars as an integer' do
+        post "/api/v1/cocktails/#{@cocktail_1.id}/rating", params: {
+          auth_token: 'BBBasdsgergn240unrfs35253',
+          stars: "three"
+        }
+
+        expect(response.status).to eq(400)
+        response_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response_body[:error]).to eq("Failed to create resource")
+        expect(response_body[:messages].first).to eq("Stars is not a number")
+      end
     end
 
-    it 'request must have params' do
-      post "/api/v1/cocktails/#{@cocktail_1.id}/rating", params: { }
+    context 'the cocktail does not yet exist in our database' do
+      it 'creates the cocktail in the database and adds the rating as well' do
+        post "/api/v1/cocktails/11423/rating/", params: {
+          auth_token: 'BBBasdsgergn240unrfs35253',
+          stars: 3
+        }
 
-      expect(response.status).to eq(400)
-      response_body = JSON.parse(response.body, symbolize_names: true)
+        expect(response.status).to eq(201)
 
-      expect(response_body[:error]).to eq("Failed to create resource")
-      expect(response_body[:messages].first).to eq("User must exist")
-      expect(response_body[:messages].second).to eq("Cocktail must exist")
-      expect(response_body[:messages].third).to eq("Stars can't be blank")
-    end
+        cocktail_1_rating = JSON.parse(response.body, symbolize_names: true)
 
-    it 'request must have stars as an integer' do
-      post "/api/v1/cocktails/#{@cocktail_1.id}/rating", params: {
-        user_id: @user_1.id,
-        cocktail_id: @cocktail_1.id,
-        stars: "three"
-      }
+        new_cocktail = Cocktail.find(cocktail_1_rating[:data][:attributes][:cocktail_id])
+        expect(new_cocktail).to be_a Cocktail
 
-      expect(response.status).to eq(400)
-      response_body = JSON.parse(response.body, symbolize_names: true)
+        rating = Rating.find(cocktail_1_rating[:data][:id])
+        expect(rating).to be_a Rating
 
-      expect(response_body[:error]).to eq("Failed to create resource")
-      expect(response_body[:messages].first).to eq("Stars is not a number")
-    end
+        excpect(rating.stars).to eq 3
+      end
 
-    it 'returns error if user and/or cocktail not found' do
-      post "/api/v1/cocktails/#{@cocktail_1.id}/rating", params: {
-        user_id: 7,
-        cocktail_id: 19,
-        stars: 3
-      }
+      it 'returns error if cocktail not found by the API' do
+        post "/api/v1/cocktails/912480267934824/rating", params: {
+          auth_token: 'BBBasdsgergn240unrfs35253',
+          stars: 3
+        }
 
-      expect(response.status).to eq(400)
-      response_body = JSON.parse(response.body, symbolize_names: true)
+        expect(response.status).to eq(400)
+        response_body = JSON.parse(response.body, symbolize_names: true)
 
-      expect(response_body[:error]).to eq("Failed to create resource")
-      expect(response_body[:messages].first).to eq("User must exist")
-      expect(response_body[:messages].second).to eq("Cocktail must exist")
+        expect(response_body[:error]).to eq("Failed to create resource")
+        expect(response_body[:messages].first).to eq("User must exist")
+        expect(response_body[:messages].second).to eq("Cocktail must exist")
+      end
     end
   end
 end
