e23bed88c3c126781f9f8f1cb68ae6539bbe8a80
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-03 21:16:51 -0600

    Refactor service class methods

--- app/services/cocktail_service.rb
+++ app/services/cocktail_service.rb
@@ -1,37 +1,28 @@
 class CocktailService
   def self.get_cocktail_details(cocktail_id)
-    response = Faraday.get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?') do |req|
-      req.params['i'] = cocktail_id
-      req.params['api_key'] = ENV['cocktail_key']
-    end
-
+    response = conn.get('lookup.php?', { i: cocktail_id })
     parse_json(response)
   end
 
   def self.random_cocktail
-    response = Faraday.get('https://www.thecocktaildb.com/api/json/v1/1/random.php') do |req|
-      req.params['api_key'] = ENV['cocktail_key']
-    end
-
+    response = conn.get('random.php')
     parse_json(response)
   end
 
   def self.search_cocktails(query)
-    response = Faraday.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?') do |req|
-      req.params['s'] = query
-      req.params['api_key'] = ENV['cocktail_key']
-    end
-
+    response = conn.get('search.php?', { s: query })
     parse_json(response)
   end
 
-  # def self.conn
-  #   Faraday.new(url: 'https://www.thecocktaildb.com/api/json/v1/1')
-  # end
+  def self.conn
+    Faraday.new(url: 'https://www.thecocktaildb.com/api/json/v1/1/') do |req|
+      req.params['api_key'] = ENV['cocktail_key']
+    end
+  end
 
   def self.parse_json(response)
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  # private_class_method :conn, :parse_json
+  private_class_method :conn, :parse_json
 end
