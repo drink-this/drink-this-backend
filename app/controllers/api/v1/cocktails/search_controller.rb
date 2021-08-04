4d39b07db843fa01839dfb444d67878c2f36452a
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-03 21:36:53 -0600

    Update search controller with before_action

--- app/controllers/api/v1/cocktails/search_controller.rb
+++ app/controllers/api/v1/cocktails/search_controller.rb
@@ -1,11 +1,9 @@
 class Api::V1::Cocktails::SearchController < Api::V1::AuthorizationController
   def index
-    user = User.find_by(google_token: params[:auth_token])
-
     if !params[:search].present?
       render json: { errors: "Couldn't find Cocktail" }, status: :not_found
     else
-      results = CocktailFacade.retrieve_search_results(params[:search], user.id)
+      results = CocktailFacade.retrieve_search_results(params[:search], current_user.id)
 
       if results == false
         render json: { errors: "Couldn't find Cocktail" }, status: :not_found
