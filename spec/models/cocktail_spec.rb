076697fa0dc182ffb9cf817e776bc4faa1124bea
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-04 22:36:05 -0600

    Update cocktail method to take user arg

--- spec/models/cocktail_spec.rb
+++ spec/models/cocktail_spec.rb
@@ -20,6 +20,8 @@ RSpec.describe Cocktail, type: :model do
 
     describe 'random_five' do
       it 'returns 5 random cocktails and ratings if available' do
+        user = create(:user)
+
         cocktail_1 = create(:cocktail, id: 11324)
         cocktail_2 = create(:cocktail, id: 11005)
         cocktail_3 = create(:cocktail, id: 11408)
@@ -35,10 +37,39 @@ RSpec.describe Cocktail, type: :model do
 
         ids = [11324, 11005, 11408, 11415, 11419, 13202, 17197, 11382, 15427, 17230, 11410, 13070]
 
-        expect(Cocktail.dashboard_five.length).to eq(5)
-        expect(Cocktail.dashboard_five.first).to be_a Cocktail
-        expect(ids).to include(Cocktail.dashboard_five.first.id) 
-        expect(Cocktail.dashboard_five.first.rating).to eq 0
+        expect(Cocktail.dashboard_five(user.id).length).to eq(5)
+        expect(Cocktail.dashboard_five(user.id).first).to be_a Cocktail
+        expect(ids).to include(Cocktail.dashboard_five(user.id).first.id) 
+        expect(Cocktail.dashboard_five(user.id).first.rating).to eq 0
+      end
+
+      it 'checks for ratings for the given user' do
+        user = create(:user)
+        user_2 = create(:user)
+
+        cocktail_1 = create(:cocktail, id: 11324)
+        cocktail_2 = create(:cocktail, id: 11005)
+        cocktail_3 = create(:cocktail, id: 11408)
+        cocktail_4 = create(:cocktail, id: 11415)
+        cocktail_5 = create(:cocktail, id: 11419)
+
+        create(:rating, cocktail: cocktail_1, user: user, stars: 3)
+        create(:rating, cocktail: cocktail_2, user: user, stars: 3)
+        create(:rating, cocktail: cocktail_3, user: user, stars: 3)
+        create(:rating, cocktail: cocktail_4, user: user, stars: 3)
+        create(:rating, cocktail: cocktail_5, user: user, stars: 3)
+
+        create(:rating, cocktail: cocktail_1, user: user_2, stars: 5)
+        create(:rating, cocktail: cocktail_2, user: user_2, stars: 5)
+        create(:rating, cocktail: cocktail_3, user: user_2, stars: 5)
+        create(:rating, cocktail: cocktail_4, user: user_2, stars: 5)
+        create(:rating, cocktail: cocktail_5, user: user_2, stars: 5)
+
+        ids = [11324, 11005, 11408, 11415, 11419]
+        expect(Cocktail.dashboard_five(user.id).length).to eq(5)
+        expect(Cocktail.dashboard_five(user.id).first).to be_a Cocktail
+        expect(ids).to include(Cocktail.dashboard_five(user.id).first.id) 
+        expect(Cocktail.dashboard_five(user.id).first.rating).to eq 3
       end
     end
   end
