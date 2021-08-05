15c1431e257418df4f57dbb24a2524c833fb3b2a
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-04 22:38:25 -0600

    Run rubocop

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::DashboardController < Api::V1::AuthorizationController
   def index
-    cocktails = Cocktail.dashboard_five
+    cocktails = Cocktail.dashboard_five(current_user.id)
     render json: CocktailDashboardSerializer.details(cocktails)
   end
 end
