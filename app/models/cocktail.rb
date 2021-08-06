f276381c332e010a6fb0751eb9ab1b0837fdda17
Zachary Green <zachjamesgreen@gmail.com>
2021-08-05 20:09:56 -0600

    Update cocktail.rb
    The onboard screen was only showing cocktails that were already rated by the user. made a change to return cocktail that have not been rated by the user

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -5,7 +5,7 @@ class Cocktail < ApplicationRecord
   def self.dashboard_five(user_id)
     left_outer_joins(:ratings)
       .select('cocktails.*, case when ratings.stars is null then 0 else ratings.stars end as rating')
-      .where('ratings.user_id is null or ratings.user_id = ?', user_id)
+      .where('ratings.user_id is null or ratings.user_id != ?', user_id)
       .order(Arel.sql('RANDOM()'))
       .limit(5)
   end
