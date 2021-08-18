f0d361bef28211f8a50da67bb8a07f247ec3014d
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-18 16:10:54 -0600

    Add VCR cassette for home_page endpoint requests

--- spec/requests/api/v1/homepage_spec.rb
+++ spec/requests/api/v1/homepage_spec.rb
@@ -1,16 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Homepage endpoint' do
-  before do
-    VCR.turn_off!
-    WebMock.allow_net_connect!
-  end
-  
-  after do
-    VCR.turn_on!
-    WebMock.disable_net_connect!
-  end
-
   describe 'GET /homepage' do
     before :each do
       User.destroy_all
@@ -18,7 +8,7 @@ RSpec.describe 'Homepage endpoint' do
       Rating.destroy_all
     end
 
-    context 'successfully returns all data needed for the homepage' do
+    context 'successfully returns all data needed for the homepage', :vcr do     
       before :each do
         @user1 = create(:user)
         @user2 = create(:user)
@@ -29,83 +19,103 @@ RSpec.describe 'Homepage endpoint' do
         end
     
         @unrated_cocktails = create_list(:cocktail, 8)
-
-        get '/api/v1/homepage', params: { auth_token: @user1.google_token }
-
-        @result = JSON.parse(response.body, symbolize_names: true)
       end
 
       it 'has the proper hash structure' do
-        expect(response.status).to eq(200)
+        VCR.use_cassette('homepage_endpoint', :match_requests_on => [:method, VCR.request_matchers.uri_without_params(:g, :s, :i)]) do
+          get '/api/v1/homepage', params: { auth_token: @user1.google_token }
 
-        expect(@result).to be_a Hash
-        expect(@result[:data][:id]).to eq nil
-        expect(@result[:data][:type]).to eq 'homepage'
+          expect(response.status).to eq(200)
 
-        details = @result[:data][:attributes]
-        expect(details).to be_a Hash
+          result = JSON.parse(response.body, symbolize_names: true)
 
-        rated = details[:rated]
-        expect(rated).to be_a Hash
-        expect(rated[:cocktails]).to be_an Array
+          expect(result).to be_a Hash
+          expect(result[:data][:id]).to eq nil
+          expect(result[:data][:type]).to eq 'homepage'
 
-        unrated = details[:unrated]
-        expect(unrated).to be_a Hash
-        expect(unrated[:cocktails]).to be_an Array
+          details = result[:data][:attributes]
+          expect(details).to be_a Hash
 
-        glass = details[:glass]
-        expect(glass).to be_a Hash
-        expect(glass[:type]).to be_a String
-        expect(glass[:cocktails]).to be_an Array
+          rated = details[:rated]
+          expect(rated).to be_a Hash
+          expect(rated[:cocktails]).to be_an Array
 
-        alcohol = details[:alcohol]
-        expect(alcohol).to be_a Hash
-        expect(alcohol[:type]).to be_a String
-        expect(alcohol[:cocktails]).to be_an Array
-      end
-      
-      it 'returns 5 rated cocktails' do
-        details = @result[:data][:attributes]
-        rated = details[:rated][:cocktails]
+          unrated = details[:unrated]
+          expect(unrated).to be_a Hash
+          expect(unrated[:cocktails]).to be_an Array
 
-        expect(rated.length).to eq 5
+          glass = details[:glass]
+          expect(glass).to be_a Hash
+          expect(glass[:type]).to be_a String
+          expect(glass[:cocktails]).to be_an Array
 
-        expected = @rated_cocktails.map(&:id)
-        rated.each do |drink|
-          expect(expected).to include(drink[:id].to_i)
+          alcohol = details[:alcohol]
+          expect(alcohol).to be_a Hash
+          expect(alcohol[:type]).to be_a String
+          expect(alcohol[:cocktails]).to be_an Array
         end
       end
+      
+      it 'returns 5 rated  and 5 unrated cocktails' do
+        VCR.use_cassette('homepage_endpoint', :match_requests_on => [:method, VCR.request_matchers.uri_without_params(:g, :s, :i)]) do
+          get '/api/v1/homepage', params: { auth_token: @user1.google_token }
 
-      it 'returns 5 random unrated cocktails' do
-        details = @result[:data][:attributes]
-        unrated = details[:unrated][:cocktails]
+          expect(response.status).to eq(200)
 
-        expect(unrated.length).to eq 5
-                
-        expected = @unrated_cocktails.map(&:id)
-        unrated.each do |drink|
-          expect(expected).to include(drink[:id].to_i)
+          result = JSON.parse(response.body, symbolize_names: true)
+          details = result[:data][:attributes]
+
+          rated = details[:rated][:cocktails]
+          expect(rated.length).to eq 5
+
+          expected = @rated_cocktails.map(&:id)
+          rated.each do |drink|
+            expect(expected).to include(drink[:id].to_i)
+          end
+
+          unrated = details[:unrated][:cocktails]
+          expect(unrated.length).to eq 5
+                  
+          expected = @unrated_cocktails.map(&:id)
+          unrated.each do |drink|
+            expect(expected).to include(drink[:id].to_i)
+          end
         end
       end
 
       it 'returns 5 cocktails with a random glass type' do
-        details = @result[:data][:attributes]
-        by_glass = details[:glass][:cocktails]
+        VCR.use_cassette('homepage_endpoint', :match_requests_on => [:method, VCR.request_matchers.uri_without_params(:g, :s, :i)]) do
+          get '/api/v1/homepage', params: { auth_token: @user1.google_token }
 
-        glass_types = ['Collins Glass', 'Highball glass', 'Old-fashioned glass', 'Champagne flute', 'Pint glass', 'Martini Glass']
-        expect(glass_types).to include details[:glass][:type]
+          expect(response.status).to eq(200)
 
-        expect(by_glass.length).to eq 5
+          result = JSON.parse(response.body, symbolize_names: true)
+
+          details = result[:data][:attributes]
+          by_glass = details[:glass][:cocktails]
+
+          glass_types = ['Collins Glass', 'Highball glass', 'Old-fashioned glass', 'Champagne flute', 'Pint glass', 'Martini Glass']
+          expect(glass_types).to include details[:glass][:type]
+
+          expect(by_glass.length).to eq 5
+        end
       end
 
       it 'returns 5 cocktails of a random type of alcohol' do
-        details = @result[:data][:attributes]
-        by_liquor = details[:alcohol][:cocktails]
+        VCR.use_cassette('homepage_endpoint', :match_requests_on => [:method, VCR.request_matchers.uri_without_params(:g, :s, :i)]) do
+          get '/api/v1/homepage', params: { auth_token: @user1.google_token }
 
-        alcohol_types = ['Gin', 'Bourbon', 'Scotch', 'Rum', 'Tequila', 'Vodka']
-        expect(alcohol_types).to include details[:alcohol][:type]
+          expect(response.status).to eq(200)
 
-        expect(by_liquor.length).to eq 5
+          result = JSON.parse(response.body, symbolize_names: true)
+          details = result[:data][:attributes]
+          by_liquor = details[:alcohol][:cocktails]
+
+          alcohol_types = ['Gin', 'Bourbon', 'Scotch', 'Rum', 'Tequila', 'Vodka']
+          expect(alcohol_types).to include details[:alcohol][:type]
+
+          expect(by_liquor.length).to eq 5
+        end
       end
     end
   end
