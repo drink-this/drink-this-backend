d0250801be8cc5ced34e408a75cd27dcd1149c08
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-18 17:28:00 -0600

    Run rubocop and repair offenses

--- app/poros/homepage.rb
+++ app/poros/homepage.rb
@@ -1,6 +1,6 @@
 class Homepage
   attr_reader :id
-  
+
   def initialize(user, options = defaults)
     @id = nil
     @user = user
@@ -22,13 +22,13 @@ class Homepage
   def glass
     glass = @glass_types.sample
     cocktails = CocktailFacade.retrieve_by_glass(glass, @user.id).sample(5)
-    { type: glass, cocktails: cocktails}
+    { type: glass, cocktails: cocktails }
   end
 
   def alcohol
     alcohol = @alcohol_types.sample
     cocktails = CocktailFacade.retrieve_search_results(alcohol, @user.id).sample(5)
-    { type: alcohol, cocktails: cocktails}
+    { type: alcohol, cocktails: cocktails }
   end
 
   def cocktail_basics(cocktail_list)
@@ -45,11 +45,11 @@ class Homepage
   def defaults
     {
       glass_types: [
-        'Collins Glass', 
-        'Highball glass', 
-        'Old-fashioned glass', 
-        'Champagne flute', 
-        'Pint glass', 
+        'Collins Glass',
+        'Highball glass',
+        'Old-fashioned glass',
+        'Champagne flute',
+        'Pint glass',
         'Martini Glass'
       ],
       alcohol_types: ['Gin', 'Bourbon', 'Scotch', 'Rum', 'Tequila', 'Vodka']
