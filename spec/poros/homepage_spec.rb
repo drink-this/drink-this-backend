8d2bd4cd8e108ba22db491a6a8acc4fe3c498144
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-18 16:12:36 -0600

    Add homepage_poro cassette

--- spec/poros/homepage_spec.rb
+++ spec/poros/homepage_spec.rb
@@ -1,58 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Homepage do
-  before do
-    VCR.turn_off!
-    WebMock.allow_net_connect!
-  end
-  
-  after do
-    VCR.turn_on!
-    WebMock.disable_net_connect!
-  end
+  it 'contains all necessary data for homepage display', :vcr do
+    VCR.use_cassette('homepage_poro', :match_requests_on => [:method, VCR.request_matchers.uri_without_params(:g, :s, :i)]) do
 
-  it 'contains all necessary data for homepage display' do
-    user = create(:user)
+      user = create(:user)
 
-    rated_cocktails = create_list(:cocktail, 5)
-    rated_cocktails.each do |cocktail|
-      Rating.create!(user: user, cocktail: cocktail, stars: 4)
-    end
+      rated_cocktails = create_list(:cocktail, 5)
+      rated_cocktails.each do |cocktail|
+        Rating.create!(user: user, cocktail: cocktail, stars: 4)
+      end
 
-    unrated_cocktails = create_list(:cocktail, 5)
+      unrated_cocktails = create_list(:cocktail, 5)
 
-    homepage = Homepage.new(user)
+      homepage = Homepage.new(user)
 
-    expect(homepage).to be_a Homepage
+      expect(homepage).to be_a Homepage
 
-    expect(homepage.rated[:cocktails]).to be_an Array
-    expect(homepage.rated[:cocktails].length).to eq 5
-    homepage.rated[:cocktails].each do |cocktail|
-      expect(cocktail).to be_a Hash
-      expect(cocktail[:rating] > 0).to be true
-    end
+      expect(homepage.rated[:cocktails]).to be_an Array
+      expect(homepage.rated[:cocktails].length).to eq 5
+      homepage.rated[:cocktails].each do |cocktail|
+        expect(cocktail).to be_a Hash
+        expect(cocktail[:rating] > 0).to be true
+      end
 
-    expect(homepage.unrated[:cocktails]).to be_an Array
-    expect(homepage.unrated[:cocktails].length).to eq 5
-    homepage.unrated[:cocktails].each do |cocktail|
-      expect(cocktail).to be_a Hash
-      expect(cocktail[:rating]).to eq 0
-    end
+      expect(homepage.unrated[:cocktails]).to be_an Array
+      expect(homepage.unrated[:cocktails].length).to eq 5
+      homepage.unrated[:cocktails].each do |cocktail|
+        expect(cocktail).to be_a Hash
+        expect(cocktail[:rating]).to eq 0
+      end
 
-    expect(homepage.glass).to be_a Hash
-    expect(homepage.glass[:type]).to be_a String
-    expect(homepage.glass[:cocktails]).to be_an Array
-    expect(homepage.glass[:cocktails].length).to eq 5
-    homepage.glass[:cocktails].each do |cocktail|
-      expect(cocktail).to be_a Hash
-    end
+      expect(homepage.glass).to be_a Hash
+      expect(homepage.glass[:type]).to be_a String
+      expect(homepage.glass[:cocktails]).to be_an Array
+      expect(homepage.glass[:cocktails].length).to eq 5
+      homepage.glass[:cocktails].each do |cocktail|
+        expect(cocktail).to be_a Hash
+      end
 
-    expect(homepage.alcohol).to be_a Hash
-    expect(homepage.alcohol[:type]).to be_a String
-    expect(homepage.alcohol[:cocktails]).to be_an Array
-    expect(homepage.alcohol[:cocktails].length).to eq 5
-    homepage.glass[:cocktails].each do |cocktail|
-      expect(cocktail).to be_a Hash
+      expect(homepage.alcohol).to be_a Hash
+      expect(homepage.alcohol[:type]).to be_a String
+      expect(homepage.alcohol[:cocktails]).to be_an Array
+      expect(homepage.alcohol[:cocktails].length).to eq 5
+      homepage.glass[:cocktails].each do |cocktail|
+        expect(cocktail).to be_a Hash
+      end
     end
   end
 end
