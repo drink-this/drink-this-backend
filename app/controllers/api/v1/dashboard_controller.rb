47ab1bcb22e5d43807c6cb277b76591f83b1637c
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-22 22:43:38 -0600

    Transfer dashboard (onboard) to use sample_unrated

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::DashboardController < Api::V1::AuthorizationController
   def index
-    cocktails = Cocktail.dashboard_five(current_user.id)
+    cocktails = Cocktail.sample_unrated(current_user.id, 5)
     render json: CocktailDashboardSerializer.details(cocktails)
   end
 end
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,17 +2,17 @@ class Cocktail < ApplicationRecord
   validates :name, :thumbnail, presence: true
 
   has_many :ratings, dependent: :destroy
-  def self.dashboard_five(_user_id)
-    #     left_outer_joins(:ratings)
-    #       .select('cocktails.*, case when ratings.stars is null then 0 else ratings.stars end as rating')
-    #       .where('ratings.user_id is null or ratings.user_id != ?', user_id)
-    #       .order(Arel.sql('RANDOM()'))
-    #       .limit(5)
-    left_outer_joins(:ratings)
-      .select('cocktails.*')
-      .order(Arel.sql('RANDOM()'))
-      .limit(5)
-  end
+  # def self.dashboard_five(_user_id)
+  #   #     left_outer_joins(:ratings)
+  #   #       .select('cocktails.*, case when ratings.stars is null then 0 else ratings.stars end as rating')
+  #   #       .where('ratings.user_id is null or ratings.user_id != ?', user_id)
+  #   #       .order(Arel.sql('RANDOM()'))
+  #   #       .limit(5)
+  #   left_outer_joins(:ratings)
+  #     .select('cocktails.*')
+  #     .order(Arel.sql('RANDOM()'))
+  #     .limit(5)
+  # end
 
   def self.top_rated(user_id)
     joins(:ratings)
