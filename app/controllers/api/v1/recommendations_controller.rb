0d6bfdecce323a51b962682db53fbcd35f072092
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-25 16:46:53 -0600

    Update controller to send API details, rather than ID

--- app/controllers/api/v1/recommendations_controller.rb
+++ app/controllers/api/v1/recommendations_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::RecommendationsController < Api::V1::AuthorizationController
   def show
     cocktail_id = RecommendationService.recommendation(current_user.id)
     details = CocktailFacade.retrieve_cocktail(current_user.id, cocktail_id)
-    render json: CocktailDetailsSerializer.details(cocktail_id, details)
+    render json: CocktailDetailsSerializer.details(details)
   end
 end
